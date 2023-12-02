815620a05dd2d8a52cac368e13f952473f4e1f05
Rasul Aslanov <resulsha@code.edu.az>
2023-12-02 18:33:36 +0400

    First

---
+++ Adapter Pattern/Adapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IStudentDataProvider
+{
+    List<string> GetStudentList();
+}
+
+public class StudentDatabase
+{
+    public List<string> FetchStudentData()
+    {
+        return new List<string> { "student 1", "stundet 2", "student 3" };
+    }
+}
+
+public class StudentDatabaseAdapter : IStudentDataProvider
+{
+    private readonly StudentDatabase studentDatabase;
+
+    public StudentDatabaseAdapter(StudentDatabase studentDatabase)
+    {
+        this.studentDatabase = studentDatabase;
+    }
+
+    public List<string> GetStudentList()
+    {
+        return studentDatabase.FetchStudentData();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        StudentDatabase studentDb = new StudentDatabase();
+
+        IStudentDataProvider studentDataProvider = new StudentDatabaseAdapter(studentDb);
+
+        List<string> studentList = studentDataProvider.GetStudentList();
+
+        Console.WriteLine("Student List:");
+        foreach (var student in studentList)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
